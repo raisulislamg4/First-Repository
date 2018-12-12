2d48071f4db7f32cf3b692b318e4b2e54f848fd3
raisulislamg4 <raisulislamg4@gmail.com>
2018-12-12 13:20:50 +0600

    add component

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'srvUp-root',
+  selector: 'app-root',
   templateUrl: './app.component.html',
   // template: `<h1>{{ title }}</h1> <p>{{ description }}  is cool </p>`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'Hello world!!!';
-  description = 'A new app';
+  description = 'Angular';
 }
